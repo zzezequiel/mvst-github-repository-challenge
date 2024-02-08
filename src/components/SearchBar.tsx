e1a825b7c87cee7feef15f71dd3dad55f516598c
zzezequiel <ezequielzvirgzdins@gmail.com>
2024-02-09 00:37:49 +0100

    add: github GQL

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,58 @@
+import { gql, useLazyQuery } from '@apollo/client';
 import { Flex, IconButton, Input } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { FiSearch } from 'react-icons/fi'
 
 interface Props { }
 
 const SearchBar = () => {
+    const SEARCH_USERS = gql`
+  query searchUsers($queryString: String!) {
+    search(query: $queryString, type: USER, first: 10) {
+      nodes {
+        ... on User {
+          login
+          name
+          avatarUrl
+        }
+      }
+    }
+  }
+`;
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchUsers, { loading, error, data }] = useLazyQuery(SEARCH_USERS);
+
+    const handleSearch = () => {
+        searchUsers({ variables: { queryString: searchTerm } });
+    };
+    console.log(data)
     return (
         <>
             <Flex>
-                <Input />
-                <IconButton aria-label='Search database' icon={<FiSearch />} />
+                <Input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search users"
+                />
+                <IconButton onClick={handleSearch} aria-label='Search database' icon={<FiSearch />} />
             </Flex>
+
+            {loading && <p>Cargando...</p>}
+            {error && <p>Error: {error.message}</p>}
+
+            {data && (
+                <ul>
+                    {data.search.nodes.map((user:any) => (
+                        <li key={user.login}>
+                            <img src={user.avatarUrl} alt="Avatar" />
+                            <a href={`https://github.com/${user.login}`}>{user.login}</a>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
     )
 }
--- src/providers/Provides.tsx
+++ src/providers/Provides.tsx
@@ -1,15 +1,34 @@
 "use client";
 
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { setContext } from '@apollo/client/link/context';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-
+    const httpLink = createHttpLink({
+        uri: 'https://api.github.com/graphql',
+      });
+      
+      const authLink = setContext((_, { headers }) => {
+        const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+        return {
+          headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+          },
+        };
+      });
+      
+      const client = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      });
     return (
-
-        <ChakraProvider>
-            {children}
-        </ChakraProvider>
+        <ApolloProvider client={client}>
+            <ChakraProvider>
+                {children}
+            </ChakraProvider>
+        </ApolloProvider>
 
     );
 }
